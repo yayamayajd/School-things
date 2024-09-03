23b23256e8556b16a70775be745e8d581c942fea
yayamaya <jdlove123123@gmail.com>
2024-09-03 20:52:58 +0200

    optimera till version1.1

--- test/src/PersonalNumber2.java
+++ test/src/PersonalNumber2.java
@@ -3,14 +3,17 @@ import java.util.Scanner;
 
 public class PersonalNumber2 {
     public static void main(String[] args) {
+        //får ssn som den första av args index, så det kan köras när man kör programet på commad line
         String ssn = args[0];
+        //varning
         if (ssn.length() != 13) {
             System.out.println("Please enter the right number");
             return;
         }
-                
+        //sätter metoderna
+        //kör metoderna
         String gender = genderCalculator(ssn);
-        String age = ageCCL(ssn);
+        String age = ageCalculator(ssn);
         String answer = String.format("your age is %s, your gender is %s", age,gender);
         System.out.println(answer);
     }
@@ -32,7 +35,7 @@ public class PersonalNumber2 {
     return gender;
     }
 
-    public static String ageCCL(String ssn) {
+    public static String ageCalculator(String ssn) {
 
     String birthYear = ssn.substring(0, 4);
     //change the type to int with Integer.parseInt(str) method
