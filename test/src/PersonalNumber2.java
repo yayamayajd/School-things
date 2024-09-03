3e471eeb6546ba69c810b8e4f227f74f3d3a4088
yayamaya <jdlove123123@gmail.com>
2024-09-03 20:41:41 +0200

    lösning av uppmaning2 version1

---
+++ test/src/PersonalNumber2.java
@@ -0,0 +1,46 @@
+
+import java.util.Scanner;
+
+public class PersonalNumber2 {
+    public static void main(String[] args) {
+        String ssn = args[0];
+        if (ssn.length() != 13) {
+            System.out.println("Please enter the right number");
+            return;
+        }
+                
+        String gender = genderCalculator(ssn);
+        String age = ageCCL(ssn);
+        String answer = String.format("your age is %s, your gender is %s", age,gender);
+        System.out.println(answer);
+    }
+
+
+    public static String genderCalculator(String ssn) {
+    String gender;
+    //take the last charactor from personal number with str.substring(index) method
+    String lastNumber = ssn.substring(11, 12);
+    //change the type to int with the Integer.parseInt(str) method
+    int genderNumber = Integer.parseInt(lastNumber);
+
+    //the if-else solution
+    if (genderNumber % 2 == 0){
+        gender = "female";
+    }else{
+        gender = "male";
+    }
+    return gender;
+    }
+
+    public static String ageCCL(String ssn) {
+
+    String birthYear = ssn.substring(0, 4);
+    //change the type to int with Integer.parseInt(str) method
+    int birthYearNumber = Integer.parseInt(birthYear);
+    int age = 2024 - birthYearNumber;
+    String nowAge = String.valueOf(age);
+    return nowAge;
+    }
+
+
+}
